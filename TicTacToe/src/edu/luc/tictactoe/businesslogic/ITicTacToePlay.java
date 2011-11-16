37c7c4e75a0cda0b799d45855ccb62a9f96e17cf
omoremi <oaatw2004@yahoo.com>
2011-11-16 14:51:06 -0600

    Interface ITicTacToePlay

---
+++ TicTacToe/src/edu/luc/tictactoe/businesslogic/ITicTacToePlay.java
@@ -0,0 +1,18 @@
+package edu.luc.tictactoe.businesslogic;
+
+import edu.luc.tictactoe.businesslogic.implementation.SelectionResult;
+
+public interface ITicTacToePlay {
+	public IPlayer getPlayerOne();
+	public IPlayer getPlayerTwo();
+	public void randomStart();
+	public void registerPlayers();
+	public void resetBoard();
+	public SelectionResult selectPosition(int pos);
+	public void setNextTurnPlayer();
+	public void setNumberOfPlays(int plays);
+	public void setPlayerOne(String name);
+	public void setPlayerTwo(String name);
+	public void switchPlayer();
+	public IPlayer whoseTurn();	
+}
